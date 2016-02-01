ba2a035455bb20a81b7dce5f10002a1534192cad
calvino151 <calvinmaxwell@hotmail.com>
2016-01-31 21:59:43 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,19 +8,15 @@ function respond() {
       botRegex = /^\/cool guy/;
       botCC = /^\/cc/;
       setcc = /^\/setcc/;
-     
+     var cc = setcc
        
-var newfuckingvar = setcc;
+var newfuckingvar = cc;
  if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage(cool());
     this.res.end();
   }
-  else if(request.text && setcc.test(request.text)) {
-this.res.writeHead(200);
-return newfuckingvar;
-this.res.end();
-}
+  
   else if(request.text && botCC.test(request.text)) {
 this.res.writeHead(200);
 postMessage(newfuckingvar);
