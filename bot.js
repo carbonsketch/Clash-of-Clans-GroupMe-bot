8e83d55cfd928174355cc4f69de1a3ab86ffe296
kuzi-moto <kuzimoto@gmail.com>
2016-02-05 17:25:59 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -94,7 +94,7 @@ function respond() {
     } else if (request.text && botMention.test(request.text)) {
         var someText = request.name;
         this.res.writeHead(200);
-        postMention(someText);
+        postMessage(someText);
         this.res.end();
     } else {
         console.log("don't care");
@@ -104,42 +104,6 @@ function respond() {
 }
 
 function postMessage(response) {
-    var botResponse, options, body, botReq;
-
-    botResponse = response
-
-    options = {
-        hostname: 'api.groupme.com',
-        path: '/v3/bots/post',
-        method: 'POST'
-    };
-
-    body = {
-        "bot_id": botID,
-        "text": botResponse
-    };
-
-    console.log('sending ' + botResponse + ' to ' + botID);
-
-    botReq = HTTPS.request(options, function(res) {
-        if (res.statusCode == 202) {
-            //neat
-        } else {
-            console.log('rejecting bad status code ' + res.statusCode);
-        }
-    });
-
-    botReq.on('error', function(err) {
-        console.log('error posting message ' + JSON.stringify(err));
-    });
-
-    botReq.on('timeout', function(err) {
-        console.log('timeout posting message ' + JSON.stringify(err));
-    });
-    botReq.end(JSON.stringify(body));
-}
-
-function postMention(response) {
     var botResponse, options, body, botReq;
 
     botResponse = response
