a275a20bbae42da3acff2f86ff1fdd42cad6be25
kuzi-moto <kuzimoto@gmail.com>
2016-02-05 17:16:55 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,8 +40,9 @@ function respond() {
     botSaveWS = /^\/setws/i; // Saves a War Sheet
     botPrintWS = /^\/ws/; // Prints the War Sheet
     botPrintCW = /^\/cw/; // Prints the ClashCaller and WarSheet together.
+    botMention - /^\/mention/i; // responds with a message mentioning the user.
 
-//commands    
+//commands
     if (request.text && botCommands.test(request.text)) {
         this.res.writeHead(200);
         postMessage("List of commands: \n \
@@ -52,7 +53,6 @@ function respond() {
                     /ws - Prints the War Sheet \n \
                     /cw - Prints the ClashCaller and War Sheet");
         this.res.end();
-    }
 // cool
     if (request.text && botCool.test(request.text)) {
         this.res.writeHead(200);
@@ -90,6 +90,12 @@ function respond() {
         this.res.writeHead(200);
         postMessage("ClashCaller: " + checkUndefined(theState) + "\n" + "War Sheet: " + checkUndefined(theState2));
         this.res.end();
+// mention
+    } else if (request.text && botMention.test(request.text)) {
+        var someText = request.name;
+        this.res.writeHead(200);
+        postMention(someText);
+        this.res.end();
     } else {
         console.log("don't care");
         this.res.writeHead(200);
@@ -98,6 +104,42 @@ function respond() {
 }
 
 function postMessage(response) {
+    var botResponse, options, body, botReq;
+
+    botResponse = response
+
+    options = {
+        hostname: 'api.groupme.com',
+        path: '/v3/bots/post',
+        method: 'POST'
+    };
+
+    body = {
+        "bot_id": botID,
+        "text": botResponse
+    };
+
+    console.log('sending ' + botResponse + ' to ' + botID);
+
+    botReq = HTTPS.request(options, function(res) {
+        if (res.statusCode == 202) {
+            //neat
+        } else {
+            console.log('rejecting bad status code ' + res.statusCode);
+        }
+    });
+
+    botReq.on('error', function(err) {
+        console.log('error posting message ' + JSON.stringify(err));
+    });
+
+    botReq.on('timeout', function(err) {
+        console.log('timeout posting message ' + JSON.stringify(err));
+    });
+    botReq.end(JSON.stringify(body));
+}
+
+function postMention(response) {
     var botResponse, options, body, botReq;
 
     botResponse = response
