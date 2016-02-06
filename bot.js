c71921457e9ad367affcfce251f7527d4aeaecd2
kuzi-moto <kuzimoto@gmail.com>
2016-02-06 00:12:00 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -147,6 +147,51 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+
+function postMention(response) {
+    var botResponse, options, body, botReq, botUrl;
+
+    botResponse = response
+    botUrl = 
+
+    options = {
+        hostname: 'api.groupme.com',
+        path: '/v3/bots/post',
+        method: 'POST'
+    };
+
+    body = {
+        "attachments":[{
+        "type":"image",
+        "url": botUrl}],
+        "bot_id": botID,
+        "text": botResponse
+    };
+
+    console.log('sending ' + botResponse + ' to ' + botID);
+
+    botReq = HTTPS.request(options, function(res) {
+        if (res.statusCode == 202) {
+            //neat
+        } else {
+            console.log('rejecting bad status code ' + res.statusCode);
+        }
+    });
+
+    botReq.on('error', function(err) {
+        console.log('error posting message ' + JSON.stringify(err));
+    });
+
+    botReq.on('timeout', function(err) {
+        console.log('timeout posting message ' + JSON.stringify(err));
+    });
+    botReq.end(JSON.stringify(body));
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
 // function to check if the ClashCaller is valid.
 function checkccLink(linkText) {
     substring = "//clashcaller.com/war";
